cec2c57ec9d8b1a8d12e3e7706eb1e9917cd7b39
MuhdNurR <102205105+MuhdNurR@users.noreply.github.com>
2023-04-12 17:21:21 +0700

    Add files via upload

---
+++ PemWeb6/P2_CekCookies/linkcookies.php
@@ -0,0 +1,3 @@
+<?php
+setcookie("variable_cookies", "ini adalah variable cookies", time() + 60);
+echo "<a href=cekcookie.php>Cek Cookies</a>";
---
+++ PemWeb6/P2_Redirect.php
@@ -0,0 +1,11 @@
+<html>
+
+<head>
+    <meta http-equiv="refresh" content="2; url=http://google.com">
+</head>
+
+<body>
+    Test Redirect
+</body>
+
+</html>
