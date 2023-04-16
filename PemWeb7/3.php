2adeed02f53a522f081f398f4b97b57697617e9e
MuhdNurR <21082010137@student.upnjatim.ac.id>
2023-04-17 02:31:32 +0700

    Delete Unnecessary

--- PemWeb7/3.php
+++ PemWeb7/3.php
@@ -19,7 +19,7 @@ $sql = "CREATE TABLE liga (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     kode VARCHAR(3) NOT NULL, 
     negara VARCHAR(30) NOT NULL, 
-    champion int(3) )";
+    champion int(3))";
 
 if (mysqli_query($conn, $sql)) {
     echo "Table created successfully";
