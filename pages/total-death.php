52cbfbcf32483a3788aa67891e1231b26e1c1b89
MuhdNurR <21082010137@student.upnjatim.ac.id>
2023-05-25 23:31:53 +0700

    Delete unnecessary

---
+++ pages/total-death.php
@@ -0,0 +1,245 @@
+<?php
+include('koneksi.php');
+$produk = mysqli_query($koneksi, "select * from tb_covid_asia");
+while ($row = mysqli_fetch_array($produk)) {
+    $nama_negara[] = $row['country'];
+
+    $query = mysqli_query($koneksi, "select sum(total_deaths) as total_deaths from tb_covid_asia where id='" . $row['id'] . "'");
+    $row = $query->fetch_array();
+    $jumlah_penderita[] = $row['total_deaths'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Spica Admin</title>
+    <!-- base:css -->
+    <link rel="stylesheet" href="../vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="../vendors/css/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="../images/favicon.png" />
+</head>
+
+<body>
+    <div class="container-scroller d-flex">
+        <!-- partial:./partials/_sidebar.php -->
+        <?php include 'header.php' ?>
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:./partials/_navbar.php -->
+            <?php include 'top-nav.php' ?>
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    <div class="row mb-5">
+                        <div class="col-sm-7">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Bar Chart</h5>
+                                    <p class="card-text">Top 10 Bar Chart By Total Death
+                                    <p>
+                                    <div class="mb-5" style="width: 100%;">
+                                        <canvas id="myChart"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-5">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Pie Chart</h5>
+                                    <p class="card-text">Top 10 Pie Chart By Total Death</p>
+                                    <div class="mb-5" style="height: 330px;">
+                                        <canvas class="mt-5" id="chart-area"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Line Chart</h5>
+                                    <p class="card-text">Top 10 Line Chart By Total Death
+                                    <p>
+                                    <div class="mb-5" style="width: 100%;">
+                                        <canvas id="myLine"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- partial:./partials/_footer.html -->
+            <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- base:js -->
+    <script src="../vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <script src="../vendors/chart.js/Chart.min.js"></script>
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="../js/off-canvas.js"></script>
+    <script src="../js/hoverable-collapse.js"></script>
+    <script src="../js/template.js"></script>
+    <!-- endinject -->
+    <!-- plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- Custom js for this page-->
+    <script src="../js/dashboard.js"></script>
+    <!-- End custom js for this page-->
+
+
+    <script>
+        var line = document.getElementById("myLine").getContext('2d');
+
+        var myLine = new Chart(line, {
+            type: 'line',
+            data: {
+                labels: <?php echo json_encode($nama_negara); ?>,
+                datasets: [{
+                    label: 'Total Kematian',
+                    data: <?php echo json_encode($jumlah_penderita); ?>,
+                    fill: false,
+                    borderColor: 'rgb(75, 192, 192)',
+                    tension: 0.1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
+
+    <script>
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: <?php echo json_encode($nama_negara); ?>,
+                datasets: [{
+                    label: 'Total Kematian',
+                    data: <?php echo json_encode($jumlah_penderita); ?>,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
+    <script>
+        var config = {
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: <?php echo json_encode($jumlah_penderita); ?>,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(255, 0, 0, 0.2)',
+                        'rgba(0, 128, 0, 0.2)',
+                        'rgba(0, 0, 255, 0.2)',
+                        'rgba(128, 128, 128, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 0, 0, 1)',
+                        'rgba(0, 128, 0, 1)',
+                        'rgba(0, 0, 255, 1)',
+                        'rgba(128, 128, 128, 1)'
+                    ],
+                    label: 'Presentase Penderita Covid Di Asia'
+                }],
+                labels: <?php echo json_encode($nama_negara); ?>
+            },
+            options: {
+                responsive: true
+            }
+        };
+
+        window.onload = function() {
+            var ctx = document.getElementById('chart-area').getContext('2d');
+            window.myPie = new Chart(ctx, config);
+        };
+
+        document.getElementById('randomizeData').addEventListener('click', function() {
+            config.data.datasets.forEach(function(dataset) {
+                dataset.data = dataset.data.map(function() {
+                    return randomScalingFactor();
+                });
+            });
+
+            window.myPie.update();
+        });
+
+        var colorNames = Object.keys(window.chartColors);
+        document.getElementById('addDataset').addEventListener('click', function() {
+            var newDataset = {
+                backgroundColor: [],
+                data: [],
+                label: 'New dataset ' + config.data.datasets.length,
+            };
+
+            for (var index = 0; index < config.data.labels.length; ++index) {
+                newDataset.data.push(randomScalingFactor());
+
+                var colorName = colorNames[index % colorNames.length];
+                var newColor = window.chartColors[colorName];
+                newDataset.backgroundColor.push(newColor);
+            }
+
+            config.data.datasets.push(newDataset);
+            window.myPie.update();
+        });
+
+        document.getElementById('removeDataset').addEventListener('click', function() {
+            config.data.datasets.splice(0, 1);
+            window.myPie.update();
+        });
+    </script>
+
+</body>
+
+</html>
