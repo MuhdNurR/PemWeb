0bac33fa8b473f9b1fd9210b7b7ca092a0e27532
MuhdNurR <21082010137@student.upnjatim.ac.id>
2023-05-28 00:31:17 +0700

    Fix

---
+++ PemWeb10/TugasPemWeb10/pages/header.php
@@ -0,0 +1,57 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.2.96/css/materialdesignicons.min.css">
+
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item sidebar-category">
+            <p>Navigation</p>
+            <span></span>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="../index.php">
+                <i class="mdi mdi-view-quilt menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+                <!-- <div class="badge badge-info badge-pill">2</div> -->
+            </a>
+        </li>
+        <li class="nav-item sidebar-category">
+            <p>Components</p>
+            <span></span>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="total-cases.php">
+                <i class="mdi mdi-chart-pie menu-icon"></i>
+                <span class="menu-title">Total Cases</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="total-death.php">
+                <i class="mdi mdi-grave-stone menu-icon"></i>
+                <span class="menu-title">Total Death</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="total-recovered.php">
+                <i class="mdi mdi-hospital-box-outline menu-icon"></i>
+                <span class="menu-title">Total Recovered</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="active-cases.php">
+                <i class="mdi mdi-virus-outline menu-icon"></i>
+                <span class="menu-title">Active Cases</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="total-tests.php">
+                <i class="mdi mdi-medical-cotton-swab menu-icon"></i>
+                <span class="menu-title">Total Testes</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="population.php">
+                <i class="mdi mdi-account-group menu-icon"></i>
+                <span class="menu-title">Population</span>
+            </a>
+        </li>
+    </ul>
+</nav>
